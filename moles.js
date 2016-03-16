3b400493ab5ac55d8fdcfb77fb12763cb717e953
Deepa Kackar <dkackar99@yahoo.com>
2016-03-16 09:07:15 -0700

    Created initial prog

---
+++ moles.js
@@ -0,0 +1,64 @@
+var ML = ML || {}
+
+
+ML.BoardModule = (function(){
+
+  var $board = $("#board"); 
+  var _boardArray [];
+  var _index;
+  var _score = 0;
+
+  function init(){
+    for (var i = 0; i < 8, i++) {
+      _boardArray[i] = false;
+    }
+  }
+
+  function getBoardVars(){
+    ML.$board = $("#board");
+  }
+
+  function renderBoard() {
+    for (var i = 0; i < 8; i++) {
+      if (_boardArray[i]) {
+         $("#hole-"+i).addClass("lit");  
+      }
+    }  
+  }
+
+  function listenMouseClick() {
+    $board.on("click",function(event) {
+      var position = event.target;
+
+      if (position.hasClass("lit") {
+        _score++;
+        position.removeClass("lit");  
+      }
+    });
+  }
+
+  function randomLit(){
+    _index = Math.floor(Math.random() * 8);
+    _boardArray[_index] = true;
+  }
+
+  function getHoleLit(i){
+    return _boardArray[i]
+  }
+
+  function update(){
+    init();
+    randomLit();
+    listenMouseClick();
+  }
+
+  function gameLoop() {
+    setInterval(update,1000);
+  }
+
+  // Return all public methods and properties
+  return {
+    gameLoop: gameLoop();
+  };
+})()
+
