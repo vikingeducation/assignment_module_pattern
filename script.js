ce1e0dc642eda35dacf54e28f3b4b9dc7e08ae71
Steven Chang <prime_pork@hotmail.com>
2016-07-23 10:50:05 +1000

    'Build a simple module which utilizes the Revealing Module Pattern - contains at least one hidden variable' - done

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 'use_strict;'
 
 var controller = {
-  init: function(){
-
+  init: function( model, view ){
   }
 };
 
 var model = {
   init: function(){
+  },
 
-  }
+  // Build a simple module which utilizes the Revealing Module Pattern and contains:
+
+  // ** Specifically, the object returned in the revealing module contains the variables and methods you want to make public but you don't actually define those functions or varaibles directly on the object...
+  // It keeps things clean by making sure the returned object contains nothing but a simple list of properties to make public.
+
+  revealingModule: (function(){
+  	// 1. At least one "hidden" variable (not accessible outside the module)
+
+  	var _privateVar = "Duh duh duhhh";
+
+  	var publicVar = "You bring light in";
+
+  	var getPublicVar = function(){
+  		return publicVar
+  	};
+
+  	var setPublicVar = function( val ){
+  		return publicVar = val
+  	};
+
+  	return {
+  		getPublicVar: getPublicVar,
+  		setPublicVar: setPublicVar
+  	};
+  })()
+
 };
 
 var view = {
@@ -19,5 +44,4 @@ var view = {
 };
 
 $(document).ready(function(){
-  controller.init();
 });
