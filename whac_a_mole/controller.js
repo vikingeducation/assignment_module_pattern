1f22459c5737fb340a23f2ccaa6c5a5c543880a6
“Matthew <matthew.hinea@gmail.com>
2016-09-15 10:15:47 -0700

    added model module

---
+++ whac_a_mole/controller.js
@@ -0,0 +1,9 @@
+var Controller = (function(Model, View) {
+
+  return {
+    // init: initializes model and view
+    // has a getMoleLocations method that calls model method
+    // userScore method that gets private variable _userScore
+  }
+
+})(Model, View)
---
+++ whac_a_mole/model.js
@@ -0,0 +1,38 @@
+var Model = (function(){
+
+  var _grid = [];
+
+  function moleLocations() {
+    var locations = []
+    for(var i = 0; i < _grid.length; i++) {
+      if (_grid[i] === "MOLE") {locations.push(i);
+      }
+    }
+    return locations
+  };
+
+  var placeMole = function(){
+    _grid[Math.floor(Math.random() * _grid.length)] = "MOLE"; 
+  };
+
+  var removeMole = function(index) {
+    _grid[index] = null;
+  };
+
+  var setNullGridVals = function() {
+    for(var i = 0; i < 8; i+=1) {
+      _grid[i] = null;
+    }
+  };
+
+
+  return {
+    moleLocations: moleLocations,
+    placeMole: placeMole;
+    removeMole: removeMole;
+    init: function() {
+      setNullGridVals();
+    }
+  }
+
+})()
