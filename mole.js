641df3a9a80c4e306d1cf2ae68f5e22bb4fb5de4
Alex Lach <alexglach@gmail.com>
2016-09-15 12:54:19 -0400

    working on extra requirements

--- mole.js
+++ mole.js
@@ -1,4 +1,10 @@
-var module = (function($) {
+var moleModule = function() {
+  
+};
+
+var gameModule = (function($) {
+
+  var score = 0;
 
   var buildBoard = function() {
     for (var i = 0; i < 8; i++) {
@@ -11,12 +17,42 @@ var module = (function($) {
 
   var addMole = function() {
     var rand = Math.floor(Math.random() * 8);
-    $('.mole').removeClass('.mole');
-    $('[data-id=' + rand + ']').addClass('.mole');
+    $('.mole').removeClass('mole');
+    $('[data-id=' + rand + ']').addClass('mole');
   };
 
+  var tic = function() {
+    setInterval(function(){
+      var rand = Math.floor(Math.random() * 5);
+      var counter = 0;
+      if (counter === rand) {
+        addMole();
+        rand = Math.floor(Math.random() * 5);
+        counter = 0;
+      }
+      counter += 1;
+    }, 200);
+  };
+
+  var setMoleListener = function() {
+    $('.board').on("click", ".mole", function() {
+      addMole();
+      score++;
+      updateScore();
+    });
+  };
+
+  var updateScore = function(){
+    $('#score').html("Your score is: " + score);
+  }
+
   return {
-    init: buildBoard
+    init: function() {
+      buildBoard();
+      tic();
+      updateScore();
+      setMoleListener();
+    }
   }
 
 })($);
@@ -24,5 +60,5 @@ var module = (function($) {
 
 
 $(document).ready(function() {
-  module.init();
+  gameModule.init();
 });
