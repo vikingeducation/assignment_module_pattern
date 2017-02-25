283de56fccc0264a35ddcd2428878ed6f281afb9
tt <ttwang.303@gmail.com>
2017-02-25 02:50:01 -0500

    stub refactor

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 var moduleOne = (function(){
-  var _hiddenVar = "i'm hiding";
-  var _hiddenMethod = function() { return 'hiding' };
+  var stub = {};
+
+  var hiddenVar = "im hiding";
+  var hiddenMethod = function() { return 'hiding' };
+
   var publicVar = 'outchea';
-  var publicVarGetter = function() { return publicVar };
-  var publicVarSetter = function(arg) { return publicVar = arg };
-  var publicMethod = function() { 
-    var a = _hiddenMethod();
-    var b = publicVarGetter();
+  stub.publicVarGetter = function() { return publicVar };
+  stub.publicVarSetter = function(arg) { return publicVar = arg };
+  stub.publicMethod = function() {
+    var a = hiddenVar;
+    var b = publicVar;
     return ('Are you ' + a + ' or ' + b + '?');
   };
 
-  return {
-    publicVarSetter: publicVarSetter,
-    publicVarGetter: publicVarGetter,
-    publicMethod: publicMethod,
-  };
+  return stub;
+
+
+
+
+  // REVEALING MODULE
+  // var _hiddenVar = "i'm hiding";
+  // var _hiddenMethod = function() { return 'hiding' };
+  // var publicVar = 'outchea';
+  // var publicVarGetter = function() { return publicVar };
+  // var publicVarSetter = function(arg) { return publicVar = arg };
+  // var publicMethod = function() { 
+  //   var a = _hiddenMethod();
+  //   var b = publicVarGetter();
+  //   return ('Are you ' + a + ' or ' + b + '?');
+  // };
+
+  // return {
+  //   publicVarSetter: publicVarSetter,
+  //   publicVarGetter: publicVarGetter,
+  //   publicMethod: publicMethod,
+  // };
 })();
