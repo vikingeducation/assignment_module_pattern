9a7632f0c5cf26fd4785e8c6d8776b2c368d04e8
Kenny Kottenstette <kennykotten@gmail.com>
2017-03-06 00:38:36 -0800

    further initial set ups

---
+++ whackAmole/main.js
@@ -0,0 +1,14 @@
+var WAM = WAM || {};
+
+WAM.MainModule (function(){
+
+  var _moleHoleCount = 8;
+
+  //20 second game length
+  var _gameTimer = 20;
+
+  return {
+
+  };
+
+}());
---
+++ whackAmole/run.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  WAM.MainModule.init();
+});
