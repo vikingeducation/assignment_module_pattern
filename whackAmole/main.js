448260e724874e3dba5f3367f2a8f278c47b0f3f
Kenny Kottenstette <kennykotten@gmail.com>
2017-03-06 00:59:19 -0800

    20 second game funcionality

--- whackAmole/main.js
+++ whackAmole/main.js
@@ -1,14 +1,40 @@
 var WAM = WAM || {};
 
-WAM.MainModule (function(){
+WAM.MainModule = (function(){
+  var _interval;
 
-  var _moleHoleCount = 8;
+  var _moleHoles = 8;
 
   //20 second game length
   var _gameTimer = 20;
+  var _score = 0;
+
+
+
+  var _gameLoop = function(){
+    _interval = setInterval(function(){
+      _gameTimer--;
+        console.log(_gameTimer)
+    }, 1000);
+
+    if (_gameTimer === 0) {
+      _stopGame();
+    }
+  };
+
+  var init = function(){
+    _gameLoop();
+  };
+
+  var _stopGame = function(){
+    console.log("game over");
+    clearInterval(_interval);
+  };
+
+
 
   return {
-
+    init: init
   };
 
 }());
