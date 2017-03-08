da6dce872d661e98aee9bb9c299b6cc3feee5982
Kenny Kottenstette <kennykotten@gmail.com>
2017-03-07 23:42:54 -0800

    begin: ensure always active moles, update a hit mole

--- whackAmole/main.js
+++ whackAmole/main.js
@@ -1,42 +1,58 @@
 var WAM = WAM || {};
 
-WAM.Main = (function($){
+WAM.Main = (function(){
   var _interval,
   _moleHoles = 8,
-  _score = 0,
+  score = 0,
   _allMoles = [],
   _gameTimer = 20; //20 second game length
+  _gameTimerElement = $("#time-left");
+  _scoreElement = $("#score");
 
-
-  var _render = function(){
-    WAM.Mole.updateMoles();
-    if (_gameTimer === 0) {
-      _stopGame();
-    } else {
-      _gameTimer--;
-    }
-  };
-
-
-  var init = function(){
-    WAM.Mole.init();
-    _render();
-    // _gameLoop();
-  };
 
   var _stopGame = function(){
     console.log("game over");
     clearInterval(_interval);
   };
 
-  var _gameLoop = function(){
+  var _renderPoints = function(domElement){
+    domElement.text(score);
+  }
+
+  var _renderTimeLeft = function(domElement){
+    domElement.text(_gameTimer);
+  }
+
+  var _renderAll = function(){
+    WAM.Mole.updateMoles();
+    _renderTimeLeft(_gameTimerElement);
+
+    if (_gameTimer === 0) {
+      _stopGame();
+    } else {
+      _gameTimer--;
+    }
+
+    _renderPoints(_scoreElement);
+  };
+
+  var _play = function(){
     _interval = setInterval(function(){
-      _render();
+      _renderAll();
     }, 1000);
   };
 
+  var init = function(){
+    WAM.Mole.init();
+    _play();
+  };
+
+  var updateScore = function(points){
+    score += points;
+  };
+
   return {
-    init: init
+    init: init, updateScore: updateScore
   };
 
 }());
--- whackAmole/mole.js
+++ whackAmole/mole.js
@@ -1,19 +1,31 @@
 var WAM = WAM || {};
 
 WAM.Mole = (function(){
+  var allMoles = [];
 
   var Mole = function(domElement){
     //the range in seconds the mole will show itself
     // which also will assign its point value,
     //the smaller the seconds value, the more points its worth
     this.seconds = WAM.getRandom(1, 4);
-    this.points = 100 / this.seconds;
+    this.points = _assignPoints(this.seconds);
     this.visible = false;
-    this.alive = true;
     this.domElement = domElement;
   };
 
-  var allMoles = [];
+  var _assignPoints = function(seconds){
+    return Math.ceil(100 / seconds);
+  }
+
+  var _createMoles = function(moleCount){
+    for (var i = 0; i < moleCount; i++){
+      var domEl = $("<div>").addClass("mole-hole")
+                            .attr("id", i);
+      var newMole = new Mole(domEl);
+
+      WAM.Mole.allMoles.push(newMole);
+    }
+  };
 
   var randomMole = function(){
     var min = 0,
@@ -22,38 +34,27 @@ WAM.Mole = (function(){
     return allMoles[moleIndex];
   };
 
-
-  var _createMoles = function(moleCount){
-    for (var i = 0; i < moleCount; i++){
-      var domEl = $("<div>").addClass("mole-hole")
-                            .attr("id", i);
-      var newMole = new Mole(domEl);
-      // console.log(); console.log(newMole) ; console.log(  )
-      WAM.Mole.allMoles.push(newMole);
-    }
-  };
-
   var _addMoleElementsToDOM = function(parentEl){
     allMoles.forEach(function(mole){
       $(parentEl).append(mole.domElement);
     });
   };
 
-  var _addClickListeners = function(){
+  var _addMoleClickListeners = function(){
     $("#game-board").on("click", ".mole-hole", function(){
       var moleId = $(this).attr('id'),
           mole = allMoles[moleId];
 
-      if (mole.alive && mole.visible) {
+      if (mole.visible) {
         mole.visible = false;
         mole.domElement.removeClass("active-mole");
+        WAM.Main.updateScore(mole.points);
       }
     });
   }
 
   Mole.prototype.render = function(){
-    if(this.alive && this.visible) {
-      console.log(this)
+    if (this.visible) {
       this.domElement.addClass("active-mole");
     } else {
       this.domElement.removeClass("active-mole");
@@ -75,7 +76,7 @@ WAM.Mole = (function(){
     var moleCount = 8, parentEl = $('#game-board');
     _createMoles(moleCount);
     _addMoleElementsToDOM(parentEl);
-    _addClickListeners();
+    _addMoleClickListeners();
 
     randomMole().visible = true;
   };
